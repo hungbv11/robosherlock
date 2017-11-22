b0a9148514ca011e1d808a7d89bd374e6c7e7969
hungbv11 <hungbv88@gmail.com>
2017-11-22 11:07:13 +0100

    Change for the tango demo

--- src/core/include/rs/view_names.h
+++ src/core/include/rs/view_names.h
@@ -58,6 +58,6 @@
 #define VIEW_SEMANTIC_MAP        "semantic_map"
 #define VIEW_SCENE               "scene"
 
-#define VIEW_CLUSTER_IMAGE        "color_image"
+#define VIEW_CLASIFIED_IMAGE        "color_image"
 
 #endif /* VIEW_NAMES_H_ */
